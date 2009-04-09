0618cb1d9c2350973fda2f951ec6ec4b22790661
elliottcable <git@elliottcable.name>
2009-04-09 12:30:06 -0800

    Have an initial implementation of `Play::Play()`

--- lib/play.rb
+++ lib/play.rb
@@ -9,7 +9,30 @@ module Play
   # This method is responsible for ferreting out a particular piece of media,
   # and playing it.
   def self.Play parameters, options = Configuration.new
+    query, season, episode = parameters
     
+    files = options[:directories].inject(Array.new) do |acc, dir|
+      options[:extensions].inject(acc) {|acc, ext| acc + Dir[File.expand_path File.join(dir, '**', ['*', ext].join('.'))] }
+    end.select do |file|
+      queries = [[query]]
+      queries << ["#{season}x", "S#{season}", "Season #{season}"] if season
+      queries << ["x#{episode}", "E#{episode}", "Episode #{episode}"] if episode
+      queries.all? {|a| a.any? {|e| file.include? e }}
+    end.reject {|file| file =~ /sample/i }
+    
+    if options[:latest]
+      # TODO: This should really parse the episode number and shit.
+      file = files.sort {|f1, f2| File.size(f1) <=> File.size(f2) }.last
+    else
+      files.each.with_index {|file, i| puts "#{i + 1}: #{file}"}
+      print "Please select a number: "
+      file = files[STDIN.gets.chomp.to_i - 1]
+    end
+    
+    args = [options[:player], file]
+    args += ['-vf', "expand=" + [:top, :bottom, :left, :right].map {|side| "-" + (options[["padding", side].join('_').intern] || 0).to_s}.join(':')]
+    args += ['-ss', options[:spot].to_s] if options[:spot]
+    system *args
   end
   
   ##
